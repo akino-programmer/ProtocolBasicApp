0b556bbf293c3ffe418a452c5da9aa1515cef982
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-03 07:35:32 +0900

    Initial Commit

---
+++ ProtocolBasicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolBasicApp
+//
+//  Created by 野口晃 on 2020/07/03.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
